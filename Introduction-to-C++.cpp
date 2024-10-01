40bb85a652d9ae4e3b61e7affad7848eb3e8f905
Neel-Kerai <neel.ramesh@strathmore.edu>
2024-10-01 19:48:13 +0300

    updated

--- Introduction-to-C++.cpp
+++ Introduction-to-C++.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){;
-cout << "Hello World, Im excited to learn C++!" << endl;
+cout << "Hello World, Im excited and glad to learn C++!" << endl;
 cout << "My name is Neel Kerai" << endl;
 return 0;
 }
